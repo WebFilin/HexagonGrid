7545c6d6aa417e8021da933fab320a44c0f8d436
WebFilin <filimigr@gmail.com>
2022-09-05 20:30:37 +0500

    Создан заголовок таблицы

--- src/Components/InfoTable/InfoTable.jsx
+++ src/Components/InfoTable/InfoTable.jsx
@@ -10,8 +10,29 @@ const InfoTable = observer(() => {
 
   return (
     <div className={style.wrapper}>
-      <p> Создано доменов: {mainDomains.length}</p>
-      <p> Элементов всего: {allElem.length}</p>
+      <table className={style.table_body}>
+        <thead className={style.table_title}>
+          <tr>
+            <th rowSpan="2">Вероятность</th>
+            <th colSpan="2">Количество доменов в решётке</th>
+            <th rowSpan="2">
+              Количество ячеек в решётке (L;N;M), из них имеющих значение 1
+            </th>
+          </tr>
+          <tr>
+            <th>Всего</th>
+            <th>Из них неодносвязных</th>
+          </tr>
+        </thead>
+
+        <tbody className={style.table_content}>
+          <tr>
+            <td>Содержимое</td>
+          </tr>
+        </tbody>
+      </table>
+      {/* <p> Создано доменов: {mainDomains.length}</p> */}
+      {/* <p> Элементов всего: {allElem.length}</p> */}
     </div>
   );
 });
